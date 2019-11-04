452627273a7cd9d83cc524a2a868c29f097db2bf
Henri Tammo <43129477+HenriTammo@users.noreply.github.com>
2019-11-04 13:11:17 +0200

    Test2 tehtud

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import {TiHeartFullOutline} from "react-icons/ti";
 
-const Test2 = () => {
-  return (
-    <div>
-      <div className={"description"}>
-        Ülesanne 2:
-        <p>
-          Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
-        </p>
-      </div>
+class Test2 extends React.Component{
+  state = {
+    count: 0
+  };
+  handleClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  }
 
-      <div style={{display:"flex", alignItems: "center"}}>
-        <div>
-          Likes: [replace me]
+  render(){
+    return (
+      <div>
+        <div className={"description"}>
+          Ülesanne 2:
+          <p>
+            Rakendus peab arvet pidama mitu korda on südame ikooni klikitud ning vastava numbri <code>likes:</code> järel kuvama.
+          </p>
         </div>
-        <TiHeartFullOutline />
-        <div>Click me</div>
+
+        <div style={{display:"flex", alignItems: "center"}}>
+          <div>
+            return {this.state.count};
+          </div>
+          <TiHeartFullOutline onClick={this.handleClick}/>
+          <div>Click me</div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default Test2;
