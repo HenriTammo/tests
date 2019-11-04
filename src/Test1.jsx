6c6156d6cac1c9ca07f8d1b862dc7b222dfe4985
Henri Tammo <43129477+HenriTammo@users.noreply.github.com>
2019-11-04 12:41:49 +0200

    test 1 done

--- src/Test1.jsx
+++ src/Test1.jsx
@@ -18,6 +18,12 @@ const ITEMS = [
 ];
 
 const Test1 = () => {
+  let phones = [];
+  let sum = 0;
+  ITEMS.forEach(item => {
+    phones.push(<div>Name: {item.name} | Price: {item.price}</div>);
+    sum += item.price;
+  });
   return (
     <div>
       <div className={"description"}>
@@ -31,9 +37,9 @@ const Test1 = () => {
         <img style={{width: 200}} src={test1Example}/>
       </div>
       <div>
-        [replace me]
+      {phones}
       </div>
-      <div><span className={"bold"}>Summa kokku:</span> [replace me]</div>
+      <div><span className={"bold"}>Summa kokku:</span> {sum}</div>
     </div>
   );
 };
