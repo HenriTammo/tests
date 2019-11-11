dad402d57de18c2bb3f4eb9f8f68e2be1c7411e8
Henri Tammo <43129477+HenriTammo@users.noreply.github.com>
2019-11-11 12:55:13 +0200

    test4 tehtud

---
+++ server/product.router.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const {Product} = require("./product.model.js");
+const mongoose = require("mongoose");
+
+router.get("/", async (req, res)=>{
+  const xs = await Product.find({});
+  res.send(xs);
+});
+
+router.get("/random", async (req, res)=>{
+  const xs = await Product.find({});
+  const r = getRandomInt(0, xs.length - 1);
+  res.status(200).send(xs[r]);
+});
+
+router.get("/similar/:productId", async (req, res)=>{
+  const product = await Product.findById(req.params.productId);
+  let brand = product.title.split(" ")[0];
+  const similar = await Product.find({title: {$regex: brand}});
+  res.status(200).json(similar);
+});
+
+module.exports = router;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
